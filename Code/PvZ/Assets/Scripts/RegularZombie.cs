61aa68d65c93febaa05f488d0fbe4efd9165e1c0
eoxv8c <purva.ekatpure@siemens.com>
2019-07-18 12:29:04 +0530

    Created Abstract class Zombies, implemented Regular Zombies class, and spawned zombies after fixed time interval

---
+++ Code/PvZ/Assets/Scripts/RegularZombie.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegularZombie : Zombie
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.5f;
+        health = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {      
+            zombieWalk();
+    }
+
+    private void updateHealth()
+    {
+       
+    }
+
+    protected override void zombieEat()
+    {
+
+    }
+
+    protected override void zombieWalk()
+    {
+        transform.Translate(-speed * Time.deltaTime, 0, 0);
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+}
---
+++ Code/PvZ/Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject Regular_Zombie;
+    public GameObject setWorld;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnZombies", 5f, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+    private void spawnSun()
+    {
+        
+    }
+
+    private void spawnZombies()
+    {
+
+        GameObject zombie = Instantiate(
+        Regular_Zombie,
+        setWorld.GetComponent<SetWorld>().lanePositions[Random.Range(0, 5)],
+        Quaternion.identity,
+        transform
+        );
+    }
+}
