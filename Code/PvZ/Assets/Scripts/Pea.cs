d507190521a9a60c2b07f36fcc146d3e5b7a8ccb
kge4uv <avanti.dorle@siemens.com>
2019-07-19 10:25:21 +0530

    Added peashooting functionality to peashooter

--- Code/PvZ/Assets/Scripts/Pea.cs
+++ Code/PvZ/Assets/Scripts/Pea.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pea : MonoBehaviour
+public class Pea : Projectile
 {
-    float speed = 3f;
+    //    public Projectile proj;
+    public float speed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(speed * Time.deltaTime, 0, 0);
+        if(transform.position.x == 10f)
+        {
+            Destroy(gameObject);
+        }
     }
+
 }
---
+++ Code/PvZ/Assets/Scripts/Peashooter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peashooter : Plant
+{
+    private float TimeInterval = 2f;
+    public GameObject Pea;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        if (timer >= TimeInterval)
+        {
+            timer = 0f;
+            shootPea();
+        }
+        
+    }
+
+    private void shootPea()
+    {
+        GameObject pea = (GameObject)Instantiate(Pea, new Vector3(transform.position.x + 0.7f, transform.position.y + 0.46f, 0), Quaternion.identity);
+    }
+}
