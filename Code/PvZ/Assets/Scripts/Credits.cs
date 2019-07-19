7e848fb1b60b2136faf3bf8397d54d3fc4e20515
Mayank Nagpurkar <mayank.nagpurkar@siemens.com>
2019-07-19 21:00:21 +0530

    Added Credits Screen

---
+++ Code/PvZ/Assets/Scripts/Credits.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BackButton()
+    {
+        Debug.Log("Back button Pressed");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
