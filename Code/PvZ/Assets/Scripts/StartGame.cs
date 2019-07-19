cfbc6f312278f3fc2ceaf7d3ea3eb87e8ebbbcbb
Aman Garg <d7euym@siemens.com>
2019-07-19 18:49:35 +0530

    Added start screen and UI elements

---
+++ Code/PvZ/Assets/Scripts/StartGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+
+    public void Playbutton()
+    {
+        Debug.Log("Play button Pressed");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitButton()
+    {
+        //Debug.Log("Quit button Pressed");
+        Application.Quit();
+    }
+
+    public void CreditsButton()
+    {
+        //Debug.Log("Credits button Pressed");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+}
