044d3b37b303fb50c35cfa6a486ef5b3bdbf7b53
Shivkumar Patil <tr0xov@siemens.com>
2019-07-20 18:00:27 +0530

    added script of changing sprite of Wallnut when health is half and applied to wallnut prefab

---
+++ Code/PvZ/Assets/Scripts/Level2/Wallnut.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallnut : Plant
+{
+    // Start is called before the first frame update
+    public Sprite halfWallnut;
+    void Start()
+    {
+        health = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health == 5)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = halfWallnut;
+        }
+    }
+}
