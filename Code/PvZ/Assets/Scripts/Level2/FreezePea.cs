a178dd3bddce0fd4ee72d0674dad53de99423bcc
kge4uv <avanti.dorle@siemens.com>
2019-07-20 16:41:09 +0530

    Added FreezePea opertions

--- Code/PvZ/Assets/Scripts/Level2/FreezePea.cs
+++ Code/PvZ/Assets/Scripts/Level2/FreezePea.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class FreezePea : Projectile
 {
+    //    public Projectile proj;
+    public float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+        if (transform.position.x == 10f)
+        {
+            Destroy(gameObject);
+        }
     }
+
 }
--- Code/PvZ/Assets/Scripts/Level2/FreezePlant.cs
+++ Code/PvZ/Assets/Scripts/Level2/FreezePlant.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class FreezePlant : Plant
 {
+    private float TimeInterval = 2f;
+    public GameObject FreezePea;
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        timer += Time.deltaTime;
+        if (timer >= TimeInterval && checkPath())
+        {
+            timer = 0f;
+            shootPea();
+        }
+
+    }
+
+    private void shootPea()
+    {
+        GameObject pea = (GameObject)Instantiate(FreezePea, new Vector3(transform.position.x + 0.7f, transform.position.y + 0.46f, 0), Quaternion.identity);
+    }
+
+    private bool checkPath()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.right, 18f, 1 << 8);
+        if (hit)
+        {
+            return hit;
+        }
+        else
+            return false;
+
     }
 }
