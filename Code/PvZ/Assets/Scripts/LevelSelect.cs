d06ba0e8159ab7bc2d4fc40cea3d39b47bfe70b0
Aman Garg <d7euym@siemens.com>
2019-07-20 12:51:03 +0530

    Added level select screen and placeholder level 2

---
+++ Code/PvZ/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LevelOne()
+    {
+        //Debug.Log("Level One Loaded");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LevelTwo()
+    {
+        //Debug.Log("LevelTwo Loaded");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void BackButton()
+    {
+        //Debug.Log("Back button Pressed");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
