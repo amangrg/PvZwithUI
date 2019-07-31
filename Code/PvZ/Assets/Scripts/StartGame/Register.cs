ded37b51dfbae3429a3fb8552fdba9f014d98db2
Aman Garg <d7euym@siemens.com>
2019-07-31 13:28:47 +0530

    First commit

---
+++ Code/PvZ/Assets/Scripts/StartGame/Register.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Register : MonoBehaviour
+{   
+    [SerializeField]
+    private InputField userName;
+    [SerializeField]
+    private InputField passWord;
+    [SerializeField]
+    private InputField confirmPassword;
+    private string form;
+    private string username;
+    private string password;
+    private string confirmpassword;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void RegisterButton()
+    {
+        bool UN = false;
+        bool PW = false;
+        bool CPW = false;
+        if (!System.IO.Directory.Exists("C:/unitydata/"))
+        {
+            System.IO.Directory.CreateDirectory("C:/unitydata/");
+        }
+        if (username != ""){
+            if (System.IO.File.Exists(@"C:/unitydata" + username + ".txt")){
+                Debug.LogWarning("Username Taken");
+            }
+            else {
+                UN = true;
+            }
+        } else{
+            Debug.LogWarning("Username field Empty");
+        }
+        if (password != ""){
+            PW = true;
+            
+        }
+        else{
+            Debug.LogWarning("Password field Empty");
+        }
+        if (confirmpassword == password){
+            CPW = true;
+
+        }
+        else{
+            Debug.LogWarning("Passwords don't match");
+        }
+        if(UN && PW && CPW){
+            form = (username + "\r\n" + password);
+            System.IO.File.WriteAllText(@"C:/unitydata/" + username + ".txt", form);
+            userName.text = "";
+            passWord.text = "";
+            confirmPassword.text = "";
+            print("Registration Successful");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {   
+        if(userName.isFocused)
+            username = userName.text;
+        if(passWord.isFocused)
+            password = passWord.text;
+        if(confirmPassword.isFocused)
+            confirmpassword = confirmPassword.text;
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (username != "" && password != "" && confirmpassword != "")
+                RegisterButton();
+        }
+        
+    }
+}
--- Code/PvZ/Assets/Scripts/StartGame/StartGame.cs
+++ Code/PvZ/Assets/Scripts/StartGame/StartGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 /*
  *startgame is first always called at begining or when game is opened.
 it initializes start ing variables and let user navigate where to go in the game (level, pause, resume, quit)
@@ -26,20 +27,47 @@ public class StartGame : MonoBehaviour
     private GameObject helpScreen = null;
     [SerializeField]
     private Button back = null;
-
+    [SerializeField]
+    private Button profile = null;
+    [SerializeField]
+    private GameObject profilePanel = null;
+    [SerializeField]
+    private Button add = null;
+    [SerializeField]
+    private InputField Name;
+    [SerializeField]
+    private Dropdown dropdown;
+    [SerializeField]
+    private Button change;
+    private string path = "C:/unitydata/";
+    private string playername;
+    private string newplayer;
+    private int selection = -1;
+    private int flag = 0;
 
     //resume.GetComponent<Button>().onClick.AddListener(() => onButtonClick(index));
 
     //Start Function is called at the start of the game always,it is used to initialize methods and variables needed at the start.
     void Start()
     {
+        dropdown.ClearOptions();
         helpScreen.SetActive(false);
+        profilePanel.SetActive(false);
         resumeLevel = PlayerPrefs.GetInt("Current Level");         //Getting the Current Level which the user was on
         if(resumeLevel == 0)
         {
             resume.interactable = false;                           //Suggesting that user was on level 0 at the start
         }
-
+        if (!System.IO.Directory.Exists(path) || !File.Exists(path + "names.txt"))
+        {
+            System.IO.Directory.CreateDirectory(path);
+            StreamWriter writer = new StreamWriter(path + "names.txt", true);
+            writer.Close();
+            PopulateDropdown();
+        } else
+        {
+            PopulateDropdown();
+        }
         resume.onClick.AddListener(delegate { Resume(); });
         play.onClick.AddListener(delegate { Playbutton(); });
         quit.onClick.AddListener(delegate { QuitButton(); });
@@ -47,14 +75,61 @@ public class StartGame : MonoBehaviour
         levels.onClick.AddListener(delegate { LevelSelect(); });
         help.onClick.AddListener(delegate { HelpScreen(); });
         back.onClick.AddListener(delegate { Back(); });
+        profile.onClick.AddListener(delegate { ProfilePanel(); });
+        add.onClick.AddListener(delegate { CreateUserData(playername); });
+        change.onClick.AddListener(delegate { ChangeUser(); });
+        
     }
+    void Update()
+    {
+        if(flag == 1)
+            selection = dropdown.value;
+        if (Name.isFocused)
+            playername = Name.text;
 
+        if(selection >= 0)
+        {
+            newplayer = dropdown.options[selection].text;
+            Debug.Log(selection);
+        }
+    }
+    private void PopulateDropdown()
+    {
+        flag = 1;
+        StreamReader reader = new StreamReader(path + "names.txt", true);
+        string []arr = File.ReadAllLines(path + "names.txt");
+        List<string> list = new List<string>(arr);
+        dropdown.AddOptions(list);
+    }
+    private void CreateUserData(string name)
+    {
+        string folderPath = path + name;
+        if (!System.IO.Directory.Exists(folderPath))
+        {
+            System.IO.Directory.CreateDirectory(folderPath);
+        } else
+        {
+            Debug.Log("User Already Exists");
+        }
+
+        dropdown.options.Add(new Dropdown.OptionData() { text = playername });
+        StreamWriter writer = new StreamWriter(path + "names.txt", true);
+        writer.WriteLine(name);
+        writer.Close();
+    }
     //Resume the last played level
     private void Resume()
     {
         SceneManager.LoadScene(resumeLevel);
     }
-
+    private void UpdatePlayer()
+    {
+        
+    }
+    private void ChangeUser()
+    {
+        profile.GetComponentInChildren<Text>().text = newplayer;
+    }
     //Start a New game
     private void Playbutton()
     {
@@ -62,7 +137,6 @@ public class StartGame : MonoBehaviour
         PlayerPrefs.DeleteKey("Current Level");
         SceneManager.LoadScene(3);
     }
-
     //Quit the Game
 
     private void QuitButton()
@@ -83,12 +157,16 @@ public class StartGame : MonoBehaviour
     {
         SceneManager.LoadScene(2);
     }
-
     private void HelpScreen()
     {
         helpScreen.SetActive(true);
     }
-
+    private void ProfilePanel()
+    {   if(profilePanel.activeSelf == false)
+            profilePanel.SetActive(true);
+        else
+            profilePanel.SetActive(false);
+    }
     private void Back()
     {
         helpScreen.SetActive(false);
